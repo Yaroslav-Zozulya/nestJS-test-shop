26e6cc93ccdda539abe8cd4bf60b7deb4000f220
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-11-08 17:49:29 +0300

    registration and authorization done

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Auth')
+@Controller('api/auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @ApiOperation({ summary: 'Registration' })
+  @ApiResponse({
+    status: 201,
+    schema: { type: 'object', properties: { token: { type: 'string' } } },
+  })
+  @Post('/registration')
+  registration(@Body() userDto: CreateUserDto) {
+    return this.authService.registration(userDto);
+  }
+
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({
+    status: 200,
+    schema: { type: 'object', properties: { token: { type: 'string' } } },
+  })
+  @Post('/login')
+  login(@Body() userDto: CreateUserDto) {
+    return this.authService.login(userDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,65 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt/dist';
+import * as bcryprjs from 'bcryptjs';
+import { User } from 'src/users/users.model';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async registration(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    if (user) {
+      throw new HttpException('Email in use', HttpStatus.CONFLICT);
+    }
+
+    const hashPassword = await bcryprjs.hash(userDto.password, 5);
+
+    const newUser = await this.userService.createUser({
+      ...userDto,
+      password: hashPassword,
+    });
+
+    return this.generateToken(newUser);
+  }
+
+  async login(userDto: CreateUserDto) {
+    const user = await this.validateUser(userDto);
+    return await this.generateToken(user);
+  }
+
+  private async generateToken(user: User) {
+    const payload = { id: user.id, email: user.email, user: user.roles };
+    return { token: this.jwtService.sign(payload) };
+  }
+
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    if (!user) {
+      throw new UnauthorizedException({
+        message: 'Email or password wrong',
+      });
+    }
+
+    const passwordEquals = await bcryprjs.compare(
+      userDto.password,
+      user.password,
+    );
+
+    if (!passwordEquals) {
+      throw new UnauthorizedException({ message: 'Email or password wrong' });
+    }
+
+    return user;
+  }
+}
